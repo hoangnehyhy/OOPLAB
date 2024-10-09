ec78d3dc0bf2cb47d1639587d4f7cfc2597fffd0
hoangnehyhy <hoang.nd235938@sis.hust.edu.vn>
2024-10-09 18:57:16 +0700

    Add files via upload

---
+++ Lab001/Matrix.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Matrix {
+    public static void main(String[] args) {
+        int N;
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        int matrix[2][N];
+        for(int i = 0; i < 2; i++){
+            for(int j = 0; j < N; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+    }
+}
